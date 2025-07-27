2aad98bded10656e2e896892ac6cb3f2921572a0
Princepatel1200 <165584676+Princepatel1200@users.noreply.github.com>
2025-07-27 22:40:06 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+# run.py
+
+from scripts.train_model import *
+from scripts.evaluate_model import *
+from scripts.predict import *
+
+if __name__ == "__main__":
+    print("Pipeline complete. Train, evaluate, and test prediction individually from scripts/.")
